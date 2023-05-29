72041dfaaaeb0fa21a83f295c4399f20506f1c0e
Manoel Mathias <dev.mathiasx@gmail.com>
2023-05-29 16:18:57 -0300

    BACK-END: Criado relacionamento entre modelos (Categoria e Curso)

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,12 +3,14 @@ import AdminJSExpress from '@adminjs/express'
 import AdminJSSequelize from '@adminjs/sequelize'
 
 import { sequelize } from '../database'
+import { adminJsResources } from './resources'
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
 export const adminJs = new AdminJS({
   databases: [sequelize],
   rootPath: '/admin',
+	resources: adminJsResources,
   branding: {
     companyName: 'OneBitFlix',
     logo: '/onebitflix.svg',
